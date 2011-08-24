8877de01d7d25d6890025e4760da55e1eaf121bd
Drew Ulmer <latortuga@gmail.com>
2011-08-24 16:59:40 -0500

    Move some documentation around and restructure core gem code

--- lib/has_magic_columns.rb
+++ lib/has_magic_columns.rb
@@ -1,2 +1,19 @@
-module HasMagicColumns
+# Has Magic Columns
+#
+# Copyright (c) 2007 Brandon Keene <bkeene AT gmail DOT com>
+#
+# Extends @attributes on a per model basis.  Column definitions exist through
+# the built-in :magic_columns association.
+#
+# class Model < ActiveRecord::Base
+#   has_magic_columns
+# end
+#
+# @model = Model.create(...) #=> @model.id == 1
+# @model.magic_columns << MagicColumn.create(:name => "first_name")
+# @model.first_name = "Brandon"
+# @model.save
+# @model = Model.find(1)
+# @model.first_name #=> "Brandon"
+module HasMagicColumns # :nodoc:
 end
