2d37396a80b5094bfe313c8b5f23cc0eaa72e3df
Drew Ulmer <latortuga@gmail.com>
2011-08-24 17:56:30 -0500

    Update railtie to act like will_paginate's
    This uses ActiveSupport.on_load :active_record which will only add this
    plugin when ActiveRecord is loaded. This means it won't be loaded if
    active record isn't loaded which is desirable so it doesn't break other
    apps.

--- lib/has_magic_columns/railtie.rb
+++ lib/has_magic_columns/railtie.rb
@@ -1,7 +1,15 @@
+require 'has_magic_columns'
+
 module HasMagicColumns
   class Railtie < Rails::Railtie
-    config.to_prepare do
-      ActiveRecord::Base.send(:include, HasMagicColumns)
+    initializer "has_magic_columns" do
+
+      # I think this is probably a ghetto way of doing this
+      # based on some limited research into other plugins. Neverhtless
+      # it's the only way I know for the moment.
+      ActiveSupport.on_load :active_record do
+        ActiveRecord::Base.send(:include, HasMagicColumns)
+      end
     end
   end
 end
