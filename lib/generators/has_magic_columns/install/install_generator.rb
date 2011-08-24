1def53cc0c043d6e95b097755c1d58293765dff5
Drew Ulmer <latortuga@gmail.com>
2011-08-24 16:58:58 -0500

    Rework the generator to work with Rails 3

---
+++ lib/generators/has_magic_columns/install/install_generator.rb
@@ -0,0 +1,22 @@
+require 'rails/generators/migration'
+
+class HasMagicColumns::Generators::InstallGenerator < ::Rails::Generators::Base
+  include Rails::Generators::Migration
+
+  source_root File.expand_path('../templates',__FILE__)
+
+  desc "Add has_magic_columns migration."
+
+  def self.next_migration_number(path)
+    unless @prev_migration_nr
+      @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+    else
+      @prev_migration_nr += 1
+    end
+    @prev_migration_nr.to_s
+  end
+
+  def add_migration
+    migration_template "migration.rb", "db/migrate/add_has_magic_columns_tables.rb"
+  end
+end
