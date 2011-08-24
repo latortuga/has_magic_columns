0bc547fd80d1062ffb85c96e478e5b4b79676871
Drew Ulmer <latortuga@gmail.com>
2011-08-24 17:42:17 -0500

    Rework install generator slightly

--- lib/generators/has_magic_columns/install/install_generator.rb
+++ lib/generators/has_magic_columns/install/install_generator.rb
@@ -1,22 +1,24 @@
-require 'rails/generators/migration'
+require 'rails/generators/base'
 
-class HasMagicColumns::Generators::InstallGenerator < ::Rails::Generators::Base
-  include Rails::Generators::Migration
+module HasMagicColumns
+  class InstallGenerator < Rails::Generators::Base
+    include Rails::Generators::Migration
 
-  source_root File.expand_path('../templates',__FILE__)
+    source_root File.expand_path('../templates',__FILE__)
 
-  desc "Add has_magic_columns migration."
+    desc "Add has_magic_columns migration."
 
-  def self.next_migration_number(path)
-    unless @prev_migration_nr
-      @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
-    else
-      @prev_migration_nr += 1
+    def self.next_migration_number(path)
+      unless @prev_migration_nr
+        @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+      else
+        @prev_migration_nr += 1
+      end
+      @prev_migration_nr.to_s
     end
-    @prev_migration_nr.to_s
-  end
 
-  def add_migration
-    migration_template "migration.rb", "db/migrate/add_has_magic_columns_tables.rb"
+    def add_migration
+      migration_template "migration.rb", "db/migrate/add_has_magic_columns_tables.rb"
+    end
   end
 end
